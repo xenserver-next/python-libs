1c9735b6a05d64a5c1de44e1945765b33ca51cf3
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2023-05-19 12:00:00 +0200

    xcp.cmd.OutputCache.fileContents: Setup keyword arguments for UTF-8 mode
    Also add line-length config for black to match pylintrc: max-line-length
    
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

---
+++ xcp/compat.py
@@ -0,0 +1,16 @@
+"""Helper module for setting up binary or UTF-8 I/O for Popen and open in Python 3.6 and newer"""
+import sys
+
+if sys.version_info >= (3, 0):
+    open_utf8 = {"encoding": "utf-8", "errors": "replace"}
+else:
+    open_utf8 = {}
+
+
+def open_defaults_for_utf8_text(args, kwargs):
+    """Setup keyword arguments for UTF-8 text mode with codec error handler to replace chars"""
+
+    mode = kwargs.get("mode", "")
+    if sys.version_info >= (3, 0) and "b" not in mode:
+        kwargs.setdefault("encoding", "utf-8")
+        kwargs.setdefault("errors", "replace")
